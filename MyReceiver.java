bb7b12012dde8676f29825de8283d839ec70c3cf
seshine jacob sebastian <seshinsebastian@gmail.com>
2016-11-24 09:24:47 +0530

    Map
    Checking code how its work

---
+++ MyReceiver.java
@@ -0,0 +1,21 @@
+package com.felight.rachana.googlemap;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+    private static final String TAG =MyReceiver.class.getSimpleName();
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+       // throw new UnsupportedOperationException("Not yet implemented");
+        Log.i(TAG,"Air Plane Mode");
+
+    }
+}
